63acd4ddd0708db9973c9d2fdf50fd8f3d1b4ecf
DooMeeR <>
2022-04-18 20:24:05 +0200

    fetch harvest and aisling costs from TFT

--- src/ninja.ml
+++ src/ninja.ml
@@ -66,17 +66,47 @@ let get_resonators league =
 let get_beasts league =
   get_items ~league "Beast"
 
+let as_tft json =
+  let as_item json =
+    let name = JSON.(json |-> "name" |> as_string) in
+    let cost = JSON.(json |-> "chaos" |> as_float) in
+    name, cost
+  in
+  JSON.(json |-> "data" |> as_array) |> List.map as_item |> String_map.of_list
+
+let get_tft filename =
+  Uri.of_string
+    ("https://raw.githubusercontent.com/The-Forbidden-Trove/tft-data-prices/master/lsc/" ^
+     filename)
+  |> http_get
+  |> as_tft
+
 let write_costs ~league ~filename =
   let currencies = get_currencies league in
   let essences = get_essences league in
   let fossils = get_fossils league in
   let resonators = get_resonators league in
   let beasts = get_beasts league in
-  let c name = String_map.find_opt name currencies in
-  let e name = String_map.find_opt name essences in
-  let f name = String_map.find_opt name fossils in
-  let r name = String_map.find_opt name resonators in
-  let b name = String_map.find_opt name beasts in
+  let harvest_crafts = get_tft "harvest.json" in
+  let services = get_tft "service.json" in
+  let wrap kind f name =
+    let result = f name in
+    (
+      match result with
+        | None ->
+            echo "%s: %s: failed to find value, will use default" kind name
+        | Some value ->
+            echo "%s: %s: %gc" kind name value
+    );
+    result
+  in
+  let c = wrap "Currency (poe.ninja)" @@ fun name -> String_map.find_opt name currencies in
+  let e = wrap "Essence (poe.ninja)" @@ fun name -> String_map.find_opt name essences in
+  let f = wrap "Fossil (poe.ninja)" @@ fun name -> String_map.find_opt name fossils in
+  let r = wrap "Resonator (poe.ninja)" @@ fun name -> String_map.find_opt name resonators in
+  let b = wrap "Beast (poe.ninja)" @@ fun name -> String_map.find_opt name beasts in
+  let h = wrap "Harvest Craft (TFT)" @@ fun name -> String_map.find_opt name harvest_crafts in
+  let s = wrap "Service (TFT)" @@ fun name -> String_map.find_opt name services in
   let cost: _ Cost.t =
     {
       transmute = c "Orb of Transmutation";
@@ -150,48 +180,48 @@ let write_costs ~league ~filename =
       eldritch_annul = c "Eldritch Orb of Annulment";
       eldritch_exalt = c "Eldritch Exalted Orb";
       eldritch_chaos = c "Eldritch Chaos Orb";
-      harvest_augment_attack = None;
-      harvest_augment_caster = None;
-      harvest_augment_chaos = None;
-      harvest_augment_cold = None;
-      harvest_augment_critical = None;
-      harvest_augment_defences = None;
-      harvest_augment_fire = None;
-      harvest_augment_life = None;
-      harvest_augment_lightning = None;
-      harvest_augment_physical = None;
-      harvest_augment_speed = None;
-      harvest_non_attack_to_attack = None;
-      harvest_non_caster_to_caster = None;
-      harvest_non_chaos_to_chaos = None;
-      harvest_non_cold_to_cold = None;
-      harvest_non_critical_to_critical = None;
-      harvest_non_defences_to_defences = None;
-      harvest_non_fire_to_fire = None;
-      harvest_non_life_to_life = None;
-      harvest_non_lightning_to_lightning = None;
-      harvest_non_physical_to_physical = None;
-      harvest_non_speed_to_speed = None;
-      harvest_reforge_attack = None;
-      harvest_reforge_caster = None;
-      harvest_reforge_chaos = None;
-      harvest_reforge_cold = None;
-      harvest_reforge_critical = None;
-      harvest_reforge_defences = None;
-      harvest_reforge_fire = None;
-      harvest_reforge_life = None;
-      harvest_reforge_lightning = None;
-      harvest_reforge_physical = None;
-      harvest_reforge_speed = None;
-      harvest_reforge_keep_prefixes = None;
-      harvest_reforge_keep_suffixes = None;
+      harvest_augment_attack = h "Augment Attack";
+      harvest_augment_caster = h "Augment Caster";
+      harvest_augment_chaos = h "Augment Chaos";
+      harvest_augment_cold = h "Augment Cold";
+      harvest_augment_critical = h "Augment Critical";
+      harvest_augment_defences = h "Augment Defence";
+      harvest_augment_fire = h "Augment Fire";
+      harvest_augment_life = h "Augment Life";
+      harvest_augment_lightning = h "Augment Lightning";
+      harvest_augment_physical = h "Augment Physical";
+      harvest_augment_speed = h "Augment Speed";
+      harvest_non_attack_to_attack = h "Remove Non-Attack Add Attack";
+      harvest_non_caster_to_caster = h "Remove Non-Caster Add Caster";
+      harvest_non_chaos_to_chaos = h "Remove Non-Chaos Add Chaos";
+      harvest_non_cold_to_cold = h "Remove Non-Cold Add Cold";
+      harvest_non_critical_to_critical = h "Remove Non-Critical Add Critical";
+      harvest_non_defences_to_defences = h "Remove Non-Defence Add Defence";
+      harvest_non_fire_to_fire = h "Remove Non-Fire Add Fire";
+      harvest_non_life_to_life = h "Remove Non-Life Add Life";
+      harvest_non_lightning_to_lightning = h "Remove Non-Lightning Add Lightning";
+      harvest_non_physical_to_physical = h "Remove Non-Physical Add Physical";
+      harvest_non_speed_to_speed = h "Remove Non-Speed Add Speed";
+      harvest_reforge_attack = h "Reforge Attack";
+      harvest_reforge_caster = h "Reforge Caster";
+      harvest_reforge_chaos = h "Reforge Chaos";
+      harvest_reforge_cold = h "Reforge Cold";
+      harvest_reforge_critical = h "Reforge Critical";
+      harvest_reforge_defences = h "Reforge Defence";
+      harvest_reforge_fire = h "Reforge Fire";
+      harvest_reforge_life = h "Reforge Life";
+      harvest_reforge_lightning = h "Reforge Lightning";
+      harvest_reforge_physical = h "Reforge Physical";
+      harvest_reforge_speed = h "Reforge Speed";
+      harvest_reforge_keep_prefixes = h "Reforge keep Prefix";
+      harvest_reforge_keep_suffixes = h "Reforge keep Suffix";
       beastcraft_aspect_of_the_avian = b "Saqawal, First of the Sky";
       beastcraft_aspect_of_the_cat = b "Farrul, First of the Plains";
       beastcraft_aspect_of_the_crab = b "Craiceann, First of the Deep";
       beastcraft_aspect_of_the_spider = b "Fenumus, First of the Night";
       beastcraft_split = b "Fenumal Plagued Arachnid";
       beastcraft_imprint = b "Craicic Chimeral";
-      aisling = None;
+      aisling = s "T4 Aisling";
     }
   in
   Cost.write_options cost filename
